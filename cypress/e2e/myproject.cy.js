a057e5280e0da14f3905c30a4e51c5a431c81773
SarahHmeidan23 <sarah.hmeidan20@gamil.com>
2023-09-18 10:27:09 -0700

    mytest

--- cypress/e2e/myproject.cy.js
+++ cypress/e2e/myproject.cy.js
@@ -1,33 +1,66 @@
+/// <reference types= "cypress" />
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
 
+Cypress.Commands.add("visiturl", () => {
+  let Websites = [
+    "https://global.almosafer.com/ar",
+    "https://global.almosafer.com/en",
+  ];
 
-/// <reference types="cypress"/>
+  let RandomIndex = Math.floor(Math.random() * Websites.length);
+  cy.visit(Websites[RandomIndex]);
 
-Cypress.on('uncaught:expection',(err ,runnable)=>{
-return false
+  cy.get(".cta__saudi").click();
+});
 
-})
-describe('Aspire testcases', () => {
-  it('randonmly enter the website arabic or english', () => {
+describe("Aspire Test cases ", () => {
+  const TheDate = new Date();
 
-    let Websites =["https://global.almosafer.com/ar","https://global.almosafer.com/en"]
+  const today_date = TheDate.getDate();
+  const expectedDepatureDate = today_date + 1;
+  const expectedreturnDate = today_date + 2;
 
-    let RandomIndex = Math.floor(Math.random()*Websites.length)
-    cy.visit(Websites[RandomIndex])
+  console.log(TheDate);
 
+  it.skip("Randomly enter the website arabic or english ", () => {
+    let Websites = [
+      "https://global.almosafer.com/ar",
+      "https://global.almosafer.com/en",
+    ];
 
-     let ArabicCities =["جدة","دبي"]
-     let ArabicRandomTndex = Math.floor(Math.random()*ArabicCities.length)
-     let englishCities =["dubai","jeddah","riyadh"]
-     let EnglishRandomTndex = Math.floor(Math.random()*englishCities.length)
+    let RandomIndex = Math.floor(Math.random() * Websites.length);
+    cy.visit(Websites[RandomIndex]);
 
+    let ArabicCities = ["جدة", "دبي"];
+    let ArabicRandomIndex = Math.floor(Math.random() * ArabicCities.length);
+    let englishCities = ["riyadh", "dubai", "jeddah"];
+    let EnglishRandomIndex = Math.floor(Math.random() * englishCities.length);
 
-    cy.get('.cta__saudi').click()
-    cy.get('#uncontrolled-tab-example-tab-hotels > .sc-dWcDbm').click()
-    if(RandomIndex==0){
-      cy.get('[data-testid="AutoCompleteInput"]').type(ArabicCities[ArabicRandomTndex])
-    }else{
-     cy.get('[data-testid="AutoCompleteInput"]').type(englishCities[EnglishRandomTndex])
-     
+    cy.get(".cta__saudi").click();
+    cy.get("#uncontrolled-tab-example-tab-hotels").click();
+    if (RandomIndex == 0) {
+      cy.get('[data-testid="AutoCompleteInput"]').type(
+        ArabicCities[ArabicRandomIndex]
+      );
+    } else {
+      cy.get('[data-testid="AutoCompleteInput"]').type(
+        englishCities[EnglishRandomIndex]
+      );
     }
   });
+
+  it("test the depature date + the return date ", () => {
+    cy.visiturl();
+
+    cy.get('[data-testid="FlightSearchBox__FromDateButton"] > .sc-eSePXt')
+      .invoke("text")
+      .then((elementText) => {
+        expect(expectedDepatureDate).to.eql(parseInt(elementText.trim()));
+      });
+  });
 });
+
